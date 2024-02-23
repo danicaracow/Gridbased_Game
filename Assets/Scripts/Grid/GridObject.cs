f0f7027eb043c4b0fe3ea64bd6b58e9e6f59f46d
CaracowGames <79090619+danicaracow@users.noreply.github.com>
2024-02-23 11:52:31 +0100

    Add food grid object type

--- Assets/Scripts/Grid/GridObject.cs
+++ Assets/Scripts/Grid/GridObject.cs
@@ -2,7 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridObject
+public class GridObject : MonoBehaviour
 {
+    public enum GridObjectType
+    {
+        Water,
+        Ground,
+        Gold,
+        Food,
+        Wood
+    }
+
+    public GridObjectType ObjectType { get; set; }
+
 
 }
--- Assets/Scripts/Grid/GridSystem.cs
+++ Assets/Scripts/Grid/GridSystem.cs
@@ -7,10 +7,11 @@ public class GridSystem : MonoBehaviour
 {
     [SerializeField] private int size;
     [SerializeField] private float scale = 0.1f;
-    [SerializeField] private GridObject[,] gridObjects; 
+    [SerializeField] private Transform[,] gridObjects; 
     [SerializeField] private int gridSize;
-    [SerializeField] private GameObject groundPrefab;
-    [SerializeField] private GameObject waterPrefab;
+    [SerializeField] private Transform groundPrefab;
+    [SerializeField] private Transform waterPrefab;
+    [SerializeField] private Transform foodPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -26,24 +27,58 @@ public class GridSystem : MonoBehaviour
         //    }
 
         //}
-        float xOffset = Random.Range(-10000f, 10000f);
-        float yOffset = Random.Range(-10000f, 10000f);
+        float xOffsetWater = Random.Range(-10000f, 10000f);
+        float yOffsetWater = Random.Range(-10000f, 10000f);
+        float xOffsetFood = Random.Range(-10000f, 10000f);
+        float yOffsetFood = Random.Range(-10000f, 10000f);
 
-        float[,] noiseMap = new float[size, size];
+        float[,] noiseMapWater = new float[size, size];
+        float[,] noiseMapFood = new float[size, size];
         for (int x = 0; x < size; x++)
             for (int y = 0; y < size; y++)
             {
-                
-                float noiseValue = Mathf.PerlinNoise(x * scale + xOffset, y * scale + yOffset);
-                noiseMap[x, y] = noiseValue;
+                float noiseValueWater = Mathf.PerlinNoise(x * scale + xOffsetWater, y * scale + yOffsetWater);
+                noiseMapWater[x, y] = noiseValueWater;
+
+                float noiseValueFood = Mathf.PerlinNoise(x * scale + xOffsetFood, y * scale + yOffsetFood);
+                noiseMapFood[x, y] = noiseValueFood;
             }
 
-        gridObjects = new GridObject[size, size];
+        gridObjects = new Transform[size, size];
         for (int x = 0; x < size; x++)
             for (int y = 0;y < size; y++)
             {
-                if (noiseMap[x, y] < 0.7f) Instantiate(groundPrefab, new Vector3(x, 0, y), Quaternion.identity);
-                else Instantiate(waterPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                if (noiseMapWater[x, y] < 0.7f)
+                {
+                    gridObjects[x, y] = Instantiate(groundPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                    gridObjects[x, y].GetComponent<GridObject>().ObjectType = GridObject.GridObjectType.Ground;
+                }
+
+                else 
+                {
+                    gridObjects[x, y] = Instantiate(waterPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                    gridObjects[x, y].GetComponent<GridObject>().ObjectType = GridObject.GridObjectType.Water;
+                }
+
+                
+            }
+
+        for (int x = 0; x < size; x++)
+            for (int y = 0; y < size; y++)
+            {
+                switch (gridObjects[x, y].GetComponent<GridObject>().ObjectType)
+                {
+                    case GridObject.GridObjectType.Water:
+                        continue;
+
+                    case GridObject.GridObjectType.Ground:
+                        if (noiseMapFood[x, y] > 0.7)
+                        {
+                            Destroy(gridObjects[x, y].gameObject);
+                            gridObjects[x, y] = Instantiate(foodPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                        }
+                        break;
+                }
             }
     }
 
